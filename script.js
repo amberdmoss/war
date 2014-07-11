8a3ce60185b27340905ee2bd72e8c3aa30c899ec
Amber Moss <amberdmoss@gmail.com>
2014-07-10 22:48:56 -0600

    added final winnner alerts

--- script.js
+++ script.js
@@ -53,9 +53,9 @@ function shuffleArray(array) {
 	}
 	//create a function (algorithm) called "war" that takes two cards as parameters, compares them and returns a winner. A tie should return false.
 	function war(card_1, card_2) {
-		if (card_1 > card_2){
+		if (card_1.number > card_2.number){
 			alert("player1 wins")
-		}else if(card_2 > card_1){
+		}else if(card_2.number > card_1.number){
 			alert("player2 wins")
 	}else {
 		alert("tie")
@@ -66,13 +66,36 @@ function shuffleArray(array) {
 		//compare the cards
 		//give the winner both cards (at end of deck)
 	function play() {
-		war(cards_player_1[0], cards_player_2[0];)
+		var round = war(cards_player_1[0], cards_player_2[0]);
+			
+		if(round == "player1 wins"){
+			cards_player_1.push(cards_player_2.shift(), cards_player_1.shift());
+				alert("player1 wins")
+		} else if (round == "player2 wins"){
+			cards_player_2.push(cards_player_1.shift(), cards_player_2.shift());
+				alert("player2 wins")
+		} else {
+			var tiebreak = war(cards_player_1[3], cards_player_2[3])
 
+			if (tiebreak === "player1 wins"){
+				for(var i = 0; i < 4; i++)
+					cards_player_1.push(cards_player_2.shift(), cards_player_1.shift());
+						alert("player1 wins")
+			}else if (tiebreak === "player2 wins"){
+				for(var i = 0; i < 4; i++)
+					cards_player_2.push(cards_player_2.shift(), cards_player_1.shift());
+						alert("player2 wins")
+			}else {
+				return false;
+			}
+		  } 
 
-		
-		//this function (defined below) will continue to the next turn
+			//this function (defined below) will continue to the next turn
 		advance();
 	}
+		
+	
+	
 	
 	function advance() {
 		//take the top two cards and display them
